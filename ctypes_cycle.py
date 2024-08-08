f5ad5d23fb0f215befdceadf4d4d59f1a34c8fb1
Xu Han <xu.han@outlook.com>
2024-08-09 02:05:34 +0800

    build module pass on Linux.

--- ctypes_cycle.py
+++ ctypes_cycle.py
@@ -6,7 +6,17 @@ def test_case():
     cpp_code = os.path.join(os.getcwd(), "csrc", "test_cpp.cpp")
     print("cpp_code: ", cpp_code)
 
-
+    output_name, output_dir = get_name_and_dir_from_output_file_path(
+        cpp_code
+    )
+    build_options = CppOptions()
+    module_builder = CppBuilder(
+        name=output_name,
+        sources=cpp_code,
+        output_dir=output_dir,
+        BuildOption=build_options)
+    
+    module_builder.build()
 
  
 if __name__ == "__main__":
