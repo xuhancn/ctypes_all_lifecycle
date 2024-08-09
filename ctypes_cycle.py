f477484a896919b19886af89aa1f5e3a167b42c9
Xu Han <xu.han@outlook.com>
2024-08-09 15:03:58 +0800

    load/close success on Linux.

--- ctypes_cycle.py
+++ ctypes_cycle.py
@@ -1,6 +1,6 @@
 import os
 from pysrc.cpp_builder import CppBuilder, CppOptions, get_name_and_dir_from_output_file_path
-
+from pysrc.module_manage import DLLWrapper
 
 def test_case():
     cpp_code = os.path.join(os.getcwd(), "csrc", "test_cpp.cpp")
@@ -20,6 +20,9 @@ def test_case():
     module_path = module_builder.get_target_file_path()
     print("module_path: ", module_path)
 
+    module = DLLWrapper(module_path)
+
+    module.close()
  
 if __name__ == "__main__":
     test_case()
